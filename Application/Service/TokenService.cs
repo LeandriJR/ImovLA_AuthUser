360cd5dd37d67e3bd2f1c1996ca80ac6c0e7a659
leandri junior <leandriwgr@gmail.com>
2025-03-22 17:32:43 -0300

    feat(services): service user e token

---
+++ Application/Service/TokenService.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using AuthService.Domain.Entities;
+
+namespace AuthService.Services
+{
+    public class TokenService
+    {
+        private readonly string _secretKey;
+        private readonly string _issuer;
+        private readonly string _audience;
+
+        public TokenService(IConfiguration configuration)
+        {
+            _secretKey = configuration["JwtSettings:SecretKey"];
+            _issuer = configuration["JwtSettings:Issuer"];
+            _audience = configuration["JwtSettings:Audience"];
+        }
+
+        public string GenerateToken(UserEntity user)
+        {
+            return null;
+        }
+    }
+}
---
+++ Application/Service/UserService.cs
@@ -0,0 +1,36 @@
+﻿using AuthService.Domain.Entities;
+using AuthUser.Application.DTO;
+using Tenant.Infrastructure;
+
+namespace AuthService.Services;
+
+public class UserService
+{
+    private readonly AppDbContext _context;
+    private readonly PasswordService _passwordService;
+
+    public UserService(AppDbContext context, PasswordService passwordService)
+    {
+        _context = context;
+        _passwordService = passwordService;
+    }
+    async public Task<UserDTO> Registrar(RegisterUserDTO registerUserDTO)
+    {   
+        string hashedPassword = _passwordService.HashPassword(registerUserDTO.Password);
+        
+        Console.WriteLine(hashedPassword);
+
+        return null;
+        var user = new UserEntity
+        {
+            UserName = registerUserDTO.UserName,
+            Email = registerUserDTO.Email,
+            Password = hashedPassword,
+            
+        };
+
+        _context.Users.Add(user);
+        await _context.SaveChangesAsync();
+        return null;
+    }
+}
