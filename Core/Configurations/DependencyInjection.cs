87d4d4263b944e2a157ba2aff0442a7eccb77e1c
leandri junior <leandriwgr@gmail.com>
2025-03-22 17:32:12 -0300

    feat(configuração): arquivo separado para injeção de dependecia

---
+++ Core/Configurations/DependencyInjection.cs
@@ -0,0 +1,24 @@
+﻿using AuthService.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Tenant.Infrastructure;
+
+namespace AuthService.Configurations
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
+
+            // Services
+            services.AddScoped<UserService>();
+            services.AddScoped<PasswordService>();
+            services.AddScoped<TokenService>();
+            
+            return services;
+        }
+    }
+}
