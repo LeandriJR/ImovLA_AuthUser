581df5c1fc5e2a21e1ada9b4a33cb674fd29e9e0
leandri junior <leandriwgr@gmail.com>
2025-03-22 17:33:32 -0300

    feat(User): DTO's para registro e usuario padrão

---
+++ Application/DTO/RegisterUserDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace AuthUser.Application.DTO;
+
+public class RegisterUserDTO
+{
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public string UserName { get; set; }
+    
+    public Guid? RoleId { get; set; }
+}
