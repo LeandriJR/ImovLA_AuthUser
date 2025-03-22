f22abd2249f3fe466ac93ede75f532ad3b749735
leandri junior <leandriwgr@gmail.com>
2025-03-22 17:31:43 -0300

    feat(configuração): Separação de arquivos de configuração

---
+++ Application/Service/PasswordService.cs
@@ -0,0 +1,25 @@
+﻿namespace AuthService.Services;
+using Microsoft.AspNetCore.Identity;
+using System;
+
+
+public class PasswordService
+{
+    private readonly PasswordHasher<string> _passwordHasher;
+
+    public PasswordService()
+    {
+        _passwordHasher = new PasswordHasher<string>();
+    }
+
+    public string HashPassword(string password)
+    {
+        return _passwordHasher.HashPassword(null, password);
+    }
+
+    public bool VerifyPassword(string hashedPassword, string providedPassword)
+    {
+        var result = _passwordHasher.VerifyHashedPassword(null, hashedPassword, providedPassword);
+        return result == PasswordVerificationResult.Success;
+    }
+}
---
+++ Core/Configurations/JwtConfiguration.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace AuthService.Configurations
+{
+    public static class JwtConfiguration
+    {
+        public static IServiceCollection AddJwtAuthentication(this IServiceCollection services, IConfiguration configuration)
+        {
+            var secretKey = Encoding.UTF8.GetBytes(configuration["Jwt:SecretKey"]);
+            var issuer = configuration["Jwt:Issuer"];
+
+            services.AddAuthentication(options =>
+                {
+                    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                })
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidIssuer = issuer,
+                        IssuerSigningKey = new SymmetricSecurityKey(secretKey),
+                        ClockSkew = TimeSpan.Zero
+                    };
+                });
+
+            return services;
+        }
+    }
+}
